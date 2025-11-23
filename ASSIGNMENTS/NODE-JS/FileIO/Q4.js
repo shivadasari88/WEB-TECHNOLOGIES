aa1792aac6db065b03e46bc97f0806b23072acf9
shivadasari88 <dasarishivasai71@gmail.com>
2025-11-23 16:59:59 +0530

    filehandling in node.js

---
+++ ASSIGNMENTS/NODE-JS/FileIO/Q4.js
@@ -0,0 +1,12 @@
+ const fs = require('fs'); 
+let arr = ["aaa", "bbb", "ccc"]; 
+// Convert array → string with pipe (|) 
+let data = arr.join("|"); 
+// Write to file names.txt 
+fs.writeFile("names.txt", data, (err) => { 
+if (err) { 
+console.log("Error writing file:", err); 
+} else { 
+console.log("names.txt written successfully!"); 
+} 
+}); 
