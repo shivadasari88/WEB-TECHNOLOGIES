9876899c19385f51a4f5ff345699170766d0fb05
shivadasari88 <dasarishivasai71@gmail.com>
2025-11-24 11:32:13 +0530

    creating first component in react

--- React/vite-project/src/App.jsx
+++ React/vite-project/src/App.jsx
@@ -1,11 +1,16 @@
 
 import './App.css'
+import ExpenseItem from './components/ExpenseItem'
 
 function App() {
 
   return (
    <div className='App'>
     <h1>Lets get started</h1>
+    <ExpenseItem/>
+    <ExpenseItem/>
+    <ExpenseItem/>
+
    </div>
   )
 }
